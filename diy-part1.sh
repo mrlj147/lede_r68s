856f36e34984ce5d3d4db5f410ba011927b286e7
yuqi <34715934+mrlj147@users.noreply.github.com>
2022-09-28 21:09:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,5 +19,7 @@
 
 
 # default on dhcp.lan.force
-ehco "uci set dhcp.lan.force=1" >>package/lean/default-settings/files/zzz-default-settings
-ehco "uci commit dhcp" >>package/lean/default-settings/files/zzz-default-settings
+sed -i '/exit 0'/d' package/lean/default-settings/files/zzz-default-settings
+echo "uci set dhcp.lan.force=1" >>package/lean/default-settings/files/zzz-default-settings
+echo "uci commit dhcp" >>package/lean/default-settings/files/zzz-default-settings
+echo "exit 0" >>package/lean/default-settings/files/zzz-default-settings
