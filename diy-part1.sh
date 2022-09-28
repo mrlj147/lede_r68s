0974402b7665f050d6f2e2eb13268c226e53c434
yuqi <34715934+mrlj147@users.noreply.github.com>
2022-09-28 21:09:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,6 +17,8 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-# default on dhcp.lan.force
+# default on dhcp.lan.force.
+sed -i '/exit 0'/d' package/lean/default-settings/files/zzz-default-settings
 echo "uci set dhcp.lan.force=1" >>package/lean/default-settings/files/zzz-default-settings
 echo "uci commit dhcp" >>package/lean/default-settings/files/zzz-default-settings
+echo "exit 0" >>package/lean/default-settings/files/zzz-default-settings
